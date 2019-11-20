dcf150eec682de3501b104b190783b98440f04c3
Fabricio Mastrocola <44126456+faberfix@users.noreply.github.com>
2019-11-20 16:49:19 -0300

    Update CuatroEnLinea.java

--- src/juego/CuatroEnLinea.java
+++ src/juego/CuatroEnLinea.java
@@ -1,27 +1,21 @@
 package juego;
 
 /**
+ * Juego Cuatro en Lí­nea
  * 
- * Juego Cuatro en Línea
- * 
- * 
- * @AXIOMAS:
- * 
- * @REGLAS:
+ * Reglas:
  * 
  * 		...
  *
  */
 public class CuatroEnLinea {
-	
-	
-	private String jugadorRojo, jugadorAmarillo, jugadorActual;
-	private Casillero [][] matrizCasilleros;
-	
+	private String jugador1, jugador2, jugadorActual;
+	private Casillero[][] casilleros;
+	private int filas,columnas;
 	
 	/**
-	 * @PRE : 'filas' y 'columnas' son mayores o iguales a 4.
-	 * @POST : empieza el juego entre el jugador que tiene fichas rojas, identificado como 
+	 * pre : 'filas' y 'columnas' son mayores o iguales a 4.
+	 * post: empieza el juego entre el jugador que tiene fichas rojas, identificado como 
 	 * 		 'jugadorRojo' y el jugador que tiene fichas amarillas, identificado como
 	 * 		 'jugadorAmarillo'. 
 	 * 		 Todo el tablero está vacío.
@@ -30,137 +24,247 @@ public class CuatroEnLinea {
 	 * @param columnas : cantidad de columnas que tiene el tablero.
 	 * @param jugadorRojo : nombre del jugador con fichas rojas.
 	 * @param jugadorAmarillo : nombre del jugador con fichas amarillas.
-	 */		
+	 */
 	public CuatroEnLinea(int filas, int columnas, String jugadorRojo, String jugadorAmarillo) {
-		
-			if(datosCorrectos(filas,columnas,jugadorRojo,jugadorAmarillo)){
-				
-				matrizCasilleros = new Casillero[columnas][filas];
-				this.jugadorRojo = jugadorRojo;
-				this.jugadorAmarillo = jugadorAmarillo;
-				jugadorActual = jugadorRojo;
-				tableroVacio();
-				
-			}
-			
+		if(datosCorrectos(filas,columnas,jugadorRojo,jugadorAmarillo)){
+			this.casilleros = new Casillero[filas][columnas];
+			this.jugador1=jugadorRojo;
+			this.jugador2=jugadorAmarillo;
+			this.jugadorActual=jugadorRojo;
+			tableroVacio();
+			this.filas=filas;
+			this.columnas=columnas;
+		}
+	}
+	private boolean datosCorrectos(int filas, int columnas, String jugadorRojo, String jugadorAmarillo){
+		if((filas>=4)&&(columnas>=4)&&(filas<=12)&&(filas<=12)&&(!jugadorRojo.isEmpty())&&(!jugadorAmarillo.isEmpty())){
+			return true;
+		}
+		else{
+			return false;
+		}
 	}
 	
-	
-	
+	private void tableroVacio(){
+		for(int i=0;i<casilleros.length;i++){
+			for(int j=0; j<casilleros[i].length;j++){
+				casilleros[i][j]=Casillero.VACIO;
+			}
+		}
+	}
+
 	/**
-	 * @POST: devuelve la cantidad máxima de fichas que se pueden apilar en el tablero.
+	 * post: devuelve la cantidad máxima de fichas que se pueden apilar en el tablero.
 	 */
 	public int contarFilas() {
 		
-		return matrizCasilleros[0].length;
+		return this.filas;
 	}
 
 	/**
-	 * @POST: devuelve la cantidad máxima de fichas que se pueden alinear en el tablero.
+	 * post: devuelve la cantidad máxima de fichas que se pueden alinear en el tablero.
 	 */
 	public int contarColumnas() {
 		
-		return matrizCasilleros.length;
+		
+		return  this.columnas;
 	}
 
 	/**
-	 * @PRE : fila está en el intervalo [1, contarFilas()],
+	 * pre : fila está en el intervalo [1, contarFilas()],
 	 * 		 columnas está en el intervalo [1, contarColumnas()].
-	 * @POST: indica qué ocupa el casillero en la posición dada por fila y columna.
+	 * post: indica qué ocupa el casillero en la posición dada por fila y columna.
 	 * 
 	 * @param fila
 	 * @param columna
 	 */
 	public Casillero obtenerCasillero(int fila, int columna) {
 		
-		
-		return matrizCasilleros[contarColumnas()-1][contarFilas()-1];
-		
+		return casilleros[fila-1][columna-1];
 	}
 	
 	/**
-	 * @PRE : el juego no terminó, columna está en el intervalo [1, contarColumnas()]
+	 * pre : el juego no terminó, columna está en el intervalo [1, contarColumnas()]
 	 * 		 y aún queda un Casillero.VACIO en la columna indicada. 
-	 * @POST: deja caer una ficha en la columna indicada.
+	 * post: deja caer una ficha en la columna indicada.
 	 * 
 	 * @param columna
 	 */
 	public void soltarFicha(int columna) {
+		int i= contarFilas()-1;
+		
+		if(!termino()){
+			while(i>=0){
+				if(this.casilleros[i][columna-1]==Casillero.VACIO){
+					this.casilleros[i][columna-1]=turnoJugador();
+					break;
+				}
+				i--;
+			}
+		}
 		
 		
-			
 	}
 	
-	
+	private Casillero turnoJugador(){
+		if(jugadorActual.equals(jugador1)){
+			jugadorActual=jugador2;
+			return Casillero.ROJO;
+		}
+		else{
+			jugadorActual=jugador1;
+			return Casillero.AMARILLO;
+		}
+	}
 	
 	/**
-	 * @POST: indica si el juego terminó porque uno de los jugadores
+	 * post: indica si el juego terminó porque uno de los jugadores
 	 * 		 ganó o no existen casilleros vacíos.
 	 */
 	public boolean termino() {
+		if(casillerosLlenos()|| hayGanador()){
+			return true;
+		}
 		
 		return false;
 	}
+	
+	private boolean casillerosLlenos(){
+		for(int i=0;i<casilleros.length;i++){
+			for(int j=0;j<casilleros[i].length;j++){
+				if(casilleros[i][j]==Casillero.VACIO){
+					return false;
+				}
+			}
+		}
+		return true; 
+	}
 
 	/**
-	 * @POST: indica si el juego terminó y tiene un ganador.
+	 * post: indica si el juego terminó y tiene un ganador.
 	 */
 	public boolean hayGanador() {
-		
-		return false;
+		if(lineaHorizontalJ1() || lineaHorizontalJ2() || lineaVerticalJ1() || lineaVerticalJ2()||diagonalDecrecienteJ1()||diagonalCrecienteJ2() || diagonalCrecienteJ1() || diagonalDecrecienteJ2()){
+			return true;
+		}
+		else{
+			return false;
+		}
 	}
 
 	/**
-	 * @PRE : el juego terminó.
-	 * @POST: devuelve el nombre del jugador que ganó el juego.
+	 * pre : el juego terminó.
+	 * post: devuelve el nombre del jugador que ganó el juego.
 	 */
 	public String obtenerGanador() {
+		if(lineaHorizontalJ1() || lineaVerticalJ1() || diagonalDecrecienteJ1() || diagonalCrecienteJ1()){
+			return jugador1;
+		}
+		else if(lineaHorizontalJ2() || lineaVerticalJ2() || diagonalCrecienteJ2() || diagonalDecrecienteJ2()){
+			return jugador2;
+		}
 		
 		return null;
 	}
-	
-	/**
-	 * @POST: crea la matriz tablero vacia
-	 * 
+	/*
+	 * @POST: Devuelve si el jugador1 ha completado una linea horizontal.
 	 */
-	private void tableroVacio(){
-		for(int i = 0; i < this.matrizCasilleros.length; i++){
-			for(int j = 0; j < this.matrizCasilleros[i].length; j++){
-				this.matrizCasilleros[i][j] = Casillero.VACIO;
+	private boolean lineaHorizontalJ1(){
+		boolean termino=false;
+		//Recorre todas las columnas, excepto tres, para comparar con las siguientes.
+		for(int i=0;i<(this.filas);i++){
+			for(int j=0;j<this.columnas-3;j++){
+				if((casilleros[i][j]==casilleros[i][j+1])&&(casilleros[i][j+1]==casilleros[i][j+2])&&(casilleros[i][j+2]==casilleros[i][j+3])&&(casilleros[i][j]==Casillero.ROJO)){
+					termino=true;
+				}
 			}
 		}
+		return termino;
 	}
-	/**
-	 * @POST: valida los datos impuesto por el juego
-	 * 
-	 * @param filas
-	 * @param columnas
-	 * @param jugadorRojo
-	 * @param jugadorAmarillo
-	 * @return
-	 */
-	private boolean datosCorrectos(int filas, int columnas, String jugadorRojo, String jugadorAmarillo){
-		
-		if (filas < 4 || columnas < 4){
-			throw new Error("Las filas y columnas deben ser mayores o iguales a 4.");
+	
+	private boolean lineaHorizontalJ2(){
+		boolean termino=false;
+		//Recorre todas las columnas, excepto tres, para comparar con las siguientes.
+		for(int i=0;i<(this.filas);i++){
+			for(int j=0;j<this.columnas-3;j++){
+				if((casilleros[i][j]==casilleros[i][j+1])&&(casilleros[i][j+1]==casilleros[i][j+2])&&(casilleros[i][j+2]==casilleros[i][j+3])&&(casilleros[i][j]==Casillero.AMARILLO)){
+					termino=true;
+				}
+			}
 		}
-		
-		if (filas > 12 || columnas > 12){
-			throw new Error("Las filas y columnas deben ser menores o iguales a 12.");
+		return termino;
+	}
+	
+	private boolean lineaVerticalJ1(){
+		boolean termino=false;
+		for(int i=0;i<this.filas-3;i++){
+			for(int j=0; j<this.columnas;j++){
+				if((casilleros[i][j]==casilleros[i+1][j])&&(casilleros[i+1][j]==casilleros[i+2][j])&&(casilleros[i+2][j]==casilleros[i+3][j])&&(casilleros[i][j]==Casillero.ROJO)){
+					termino=true;
+				}
+			}
 		}
-		
-		if (jugadorRojo.isEmpty()){
-			throw new Error("Falta el nombre del jugador Rojo.");
+		return termino;
+	}
+	
+	private boolean lineaVerticalJ2(){
+		boolean termino=false;
+		for(int i=0;i<this.filas-3;i++){
+			for(int j=0; j<this.columnas;j++){
+				if((casilleros[i][j]==casilleros[i+1][j])&&(casilleros[i+1][j]==casilleros[i+2][j])&&(casilleros[i+2][j]==casilleros[i+3][j])&&(casilleros[i][j]==Casillero.AMARILLO)){
+					termino=true;
+				}
+			}
 		}
-		
-		if (jugadorAmarillo.isEmpty()){
-			throw new Error("Falta el nombre del jugador Amarillo.");
+		return termino;
+	}
+	
+	private boolean diagonalDecrecienteJ1(){
+		boolean termino=false;
+		for(int i=0;i<this.filas-3;i++){
+			for(int j=0;j<this.columnas-3;j++){
+				if((casilleros[i][j]==casilleros[i+1][j+1])&&(casilleros[i+1][j+1]==casilleros[i+2][j+2])&&(casilleros[i+2][j+2]==casilleros[i+3][j+3])&&(casilleros[i][j]==Casillero.ROJO)){
+					termino=true;
+				}
+			}
 		}
-		
-		if (jugadorRojo.equalsIgnoreCase(jugadorAmarillo)){
-			throw new Error("No pueden tener el mismo los jugadores.");
+		return termino;
+	}
+	
+	private boolean diagonalCrecienteJ2(){
+		boolean termino=false;
+		for(int i=this.filas-1;i>=3;i--){
+			for(int j=0;j<this.columnas-3;j++){
+				if((casilleros[i][j]==casilleros[i-1][j+1])&&(casilleros[i-1][j+1]==casilleros[i-2][j+2])&&(casilleros[i-2][j+2]==casilleros[i-3][j+3])&&(casilleros[i][j]==Casillero.AMARILLO)){
+					termino=true;
+				}
+			}
 		}
-		
-		return true;
+		return termino;
+	}
+	
+	private boolean diagonalDecrecienteJ2(){
+		boolean termino=false;
+		for(int i=0;i<this.filas-3;i++){
+			for(int j=0;j<this.columnas-3;j++){
+				if((casilleros[i][j]==casilleros[i+1][j+1])&&(casilleros[i+1][j+1]==casilleros[i+2][j+2])&&(casilleros[i+2][j+2]==casilleros[i+3][j+3])&&(casilleros[i][j]==Casillero.AMARILLO)){
+					termino=true;
+				}
+			}
+		}
+		return termino;
+	}
+	
+	private boolean diagonalCrecienteJ1(){
+		boolean termino=false;
+		for(int i=this.filas-1;i>=3;i--){
+			for(int j=0;j<this.columnas-3;j++){
+				if((casilleros[i][j]==casilleros[i-1][j+1])&&(casilleros[i-1][j+1]==casilleros[i-2][j+2])&&(casilleros[i-2][j+2]==casilleros[i-3][j+3])&&(casilleros[i][j]==Casillero.ROJO)){
+					termino=true;
+				}
+			}
+		}
+		return termino;
 	}
 }
+
